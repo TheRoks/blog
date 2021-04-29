dd85ef8665f7580e3716c7b08a0f0915318bdb25
NuKeeper <nukeeper@nukeeper.com>
2021-04-29 14:50:32 +0200

    added webfont loading

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,13 +56,40 @@ module.exports = {
         ],
       },
     },
+    // {
+    //   resolve: 'gatsby-plugin-web-font-loader',
+    //   options: {
+    //     google: {
+    //       families: ['Droid Sans', 'Droid Serif']
+    //     }
+    //   }
+    // },
     {
-      resolve: 'gatsby-plugin-web-font-loader',
+      resolve: "gatsby-omni-font-loader",
       options: {
-        google: {
-          families: ['Droid Sans', 'Droid Serif']
-        }
-      }
+
+        /* Enable font loading listener to handle FOUC */
+        enableListener: true,
+        /* Preconnect URL-s. This example is for Google Fonts */
+        preconnect: ["https://fonts.gstatic.com"],
+        /* Self-hosted fonts config. Add font files and font CSS files to "static" folder */
+        // custom: [
+        //   {
+        //     /* Exact name of the font as defied in @font-face CSS rule */
+        //     name: ["Font Awesome 5 Brands", "Font Awesome 5 Free"],
+        //     /* Path to the font CSS file inside the "static" folder with @font-face definition */
+        //     file: "/fonts/fontAwesome/css/all.min.css",
+        //   },
+        // ],
+        /* Web fonts. File link should point to font CSS file. */
+        web: [{
+            /* Exact name of the font as defied in @font-face CSS rule */
+            name: "Staatliches",
+            /* URL to the font CSS file with @font-face definition */
+            file: "https://fonts.googleapis.com/css2?family=Staatliches",
+          },
+        ],
+      },
     },
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
